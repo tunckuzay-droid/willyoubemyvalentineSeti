01721915dd4d48f3e127660f06a88cb357bd90c7
tunckuzay-droid <tunc.kuzay@ozu.edu.tr>
2026-01-31 13:50:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th,2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come get your gift, I will be waiting for you at home!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
